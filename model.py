11051fa6d28590bfae40a952f0e7c48196f8f83d
black1172 <black.1172@buckeyemail.osu.edu>
2025-07-05 18:03:56 -0400

    model intial commit

---
+++ model.py
@@ -0,0 +1,12 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+class CIFAR10CNN(nn.Module):
+    def __init__(self):
+        super(CIFAR10CNN, self).__init__()
+        # TODO: Add convolutional layers here
+        
+    def forward(self, x):
+        # TODO: Define how data flows through the network
+        pass
