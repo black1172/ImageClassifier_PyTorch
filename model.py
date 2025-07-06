7aeb5f57c248a72eea2083dc79ccc3e13b610ada
black1172 <black.1172@buckeyemail.osu.edu>
2025-07-06 12:49:05 -0400

    2nd image pass in model

--- model.py
+++ model.py
@@ -5,7 +5,11 @@ import torch.nn.functional as F
 class CIFAR10CNN(nn.Module):
     def __init__(self):
         super(CIFAR10CNN, self).__init__()
-        # TODO: Add convolutional layers here
+        self.conv1 = nn.Conv2d(3, 32, 3)    # 3×3 pattern detection
+        self.pool1 = nn.MaxPool2d(2, 2)     # 2×2 compression
+
+        self.conv2 = nn.Conv2d(32, 64, 3)   # taking our 32 maps and detecting 64 further features
+        self.pool2 = nn.MaxPool2d(2, 2)     # 2×2 compression
         
     def forward(self, x):
         # TODO: Define how data flows through the network
